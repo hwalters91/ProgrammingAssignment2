5b0fcb507421455b3c1f17e0d5c28caeb059bf39
hwalters91 <hwalters91@gmail.com>
2015-09-26 12:11:11 -0500

    RprogrammingAssigment2_walters

--- cachematrix.R
+++ cachematrix.R
@@ -1,26 +1,32 @@
 ##this function computes the inverse of a square matrix and caches 
-## creates a special matrix that caches the inverse
+##also creates a special matrix that caches the inverse
 makeCacheMatrix <- function(x = matrix()) {
   set <- function(y) {
     x <<- y
     i <<- NULL
+    
   }
   get <- function() x 
   setinv <- function(reverse) i <<- reverse
   getinv <- function() i
   list( set = set, get= get, setinv = setinv, getinv = getinv)
 }
+
 ## computes the inverse of thee special matrix returned from the makeCacheMatrix.
 ## if the inverse has already been calculated then cacheSolve retrieves the inverse of the cache
 cacheSolve <- function(x, ...) {
+  
   invFunc <- x$getinv()
   if(!is.null(invFunc)){
     message("getting cached data")
     return(invFunc)
+    
   }
   data <- x$get()
   invFunc <- solve(data, ...)
+  
   x$setinv(invFunc)
   invFunc
 }
+
 
